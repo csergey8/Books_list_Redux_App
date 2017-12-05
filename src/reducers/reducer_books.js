907158cf142aca671f778d526a967de5b17b985e
csergey8 <c.sergey@protonmail.com>
2017-12-05 13:59:42 +0200

    add another redux functionality

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: Good Parts'},
-        { title: 'Harry Potter'},
-        { title: 'The Dark Tower'},
-        { title: 'Eloquent Ruby'}
+        { title: 'Javascript: Good Parts', pages: 101},
+        { title: 'Harry Potter', pages: 68},
+        { title: 'The Dark Tower', pages:350},
+        { title: 'Eloquent Ruby', pages: 1}
     ]
 }
